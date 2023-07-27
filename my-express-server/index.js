a5997f1e274912decbe737cd478a9070c81b4535
Sasukexo <aswinash1002@gmail.com>
2023-07-27 18:40:37 +0530

    changed port no

--- my-express-server/index.js
+++ my-express-server/index.js
@@ -1,7 +1,7 @@
 //jshint esversion:6
 
 const express =require('express');
-
+const port=3000;
 const app=express();
 
 app.get('/', function(req, res) {
@@ -9,6 +9,6 @@ app.get('/', function(req, res) {
     
     
 });
-app.listen(3000, function(){
+app.listen(port, function(){
     console.log('Server started on port 3000');
 });
