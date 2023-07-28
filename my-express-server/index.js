7266d5fb133518ab057dea445465aeb30047d567
Sasukexo <aswinash1002@gmail.com>
2023-07-28 19:19:04 +0530

    routes

--- my-express-server/index.js
+++ my-express-server/index.js
@@ -10,6 +10,18 @@ app.get('/', function(req, res) {
     
     
 });
+app.get('/about', function(req, res) {
+    res.send('<h1>Hello! I am Aswin </h1>');
+    
+    
+});
+app.get('/contact', function(req, res) {
+    res.send('<h1>Contact me throught mail @ aswinash1002@gmail.com</h1>');
+    
+    
+});
+
+
 app.listen(port, function(){
     console.log('Server started on port 3000');
 });
